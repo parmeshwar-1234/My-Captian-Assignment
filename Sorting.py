5eff778adcc1252120573fc1bc93cc770c115afe
parmeshwar-1234 <70964810+parmeshwar-1234@users.noreply.github.com>
2022-05-08 22:17:18 +0530

    Add files via upload

---
+++ Sorting.py
@@ -0,0 +1,12 @@
+Numbers = [10, -417, -4, 5, 136, -932]
+incrementor = 0
+  
+while(incrementor < len(Numbers)):
+      
+
+    if Numbers[incrementor] >= 0:
+        print(Numbers[incrementor], end = " ")   # That 'end' is to make a space between the new value 
+
+    incrementor=incrementor+1
+     
+
